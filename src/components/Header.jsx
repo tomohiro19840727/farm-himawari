a500f51bc523dd34ad70069008f8887bda52e48b
tomohiro19840727 <tomohirofarm@gmail.com>
2024-12-21 14:44:00 +0900

    34

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
   
   return (    
     <header class="mb-4 flex items-center justify-between py-4 md:py-8 ml-10">      
-          <a href="/" class="text-black-800 inline-flex items-center gap-2.5 text-3xl font-bold md:text-4xl" aria-label="logo">
+          <a href="/" class="text-black-800 inline-flex items-center gap-2.5 text-3xl font-bold md:text-5xl" aria-label="logo">
             <svg width="95" height="94" viewBox="0 0 95 94" class="h-auto w-6 text-indigo-500" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
               <path d="M96 0V47L48 94H0V47L48 0H96Z" />
             </svg>
@@ -32,16 +32,6 @@ const Header = () => {
             <span class="absolute inset-0 w-full h-full duration-300 delay-300 bg-gray-900 opacity-0 group-hover:opacity-100"></span>
               <span class="relative transition-colors duration-300 delay-200 group-hover:text-white ease text-indigo-500">会社概要</span>
              </Link>
-
-
-            {/* <Link to="/careers" class="relative px-5 py-3 overflow-hidden font-medium text-gray-600 bg-gray-100 border border-gray-100 rounded-lg shadow-inner group">
-            <span class="absolute top-0 left-0 w-0 h-0 transition-all duration-200 border-t-2 border-gray-600 group-hover:w-full ease"></span>
-            <span class="absolute bottom-0 right-0 w-0 h-0 transition-all duration-200 border-b-2 border-gray-600 group-hover:w-full ease"></span>
-            <span class="absolute top-0 left-0 w-full h-0 transition-all duration-300 delay-200 bg-gray-600 group-hover:h-full ease"></span>
-            <span class="absolute bottom-0 left-0 w-full h-0 transition-all duration-300 delay-200 bg-gray-600 group-hover:h-full ease"></span>
-            <span class="absolute inset-0 w-full h-full duration-300 delay-300 bg-gray-900 opacity-0 group-hover:opacity-100"></span>
-              <span class="relative transition-colors duration-300 delay-200 group-hover:text-white ease text-indigo-500">採用情報</span>
-             </Link>       */}
           </nav>
           
           <Link to="/contact" class="hidden rounded-lg bg-gray-200 px-8 py-3 text-center text-sm font-semibold text-gray-500 outline-none ring-indigo-300 transition duration-100 hover:bg-gray-300 focus-visible:ring active:text-gray-700 md:text-base lg:inline-block mr-10">お問い合わせ</Link>
